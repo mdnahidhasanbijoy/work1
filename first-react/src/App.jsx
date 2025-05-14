a5a918ece42423553e1397720bbe7744f2756d27
unknown <nahidhasanbijoy26@gmail.com>
2025-05-15 01:16:13 +0600

    modified

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -8,24 +8,24 @@ import './App.css'
 
 function App() {
   
-  const StudentInfo =[
-  {
-    name: 'rakib',
-    roll: 1250
-  },
-  {
-    name: 'rohim',
-    roll: 1260
-  },
-  {
-    name: 'korim',
-    roll: 1290
-  },
-  {
-    name: 'zabbar',
-    roll: 1300
-  }
-  ]
+  // const StudentInfo =[
+  // {
+  //   name: 'rakib',
+  //   roll: 1250
+  // },
+  // {
+  //   name: 'rohim',
+  //   roll: 1260
+  // },
+  // {
+  //   name: 'korim',
+  //   roll: 1290
+  // },
+  // {
+  //   name: 'zabbar',
+  //   roll: 1300
+  // }
+  // ]
 
   return (
     <div>
@@ -34,25 +34,25 @@ function App() {
      <Student name="Rohim" roll="1201"/>
     <Student name="Korim" roll="1202"/>
     <Student name="Josim" roll="1203"/> */}
-      {
+      {/* {
         StudentInfo.map((item)=>{
           return <Student item={item}></Student>
          
         })
-      }
-      
+      } */}
+      <Student name="nahid hasan" roll="16"/>
 
     </div>
   );
 }
 
- function Student ({item}) {
+ function Student (props) {
   // const {name, roll} = item
   return (
     <>
       <div className='info'>
-        <h2>Name: {item.name} </h2>
-        <h3>Roll: {item.roll} </h3>
+        <h2>Name: {props.name} </h2>
+        <h3>Roll: {props.roll} </h3>
       </div>
     </>
   )
