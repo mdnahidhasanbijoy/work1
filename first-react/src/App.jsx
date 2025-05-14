be41036d372ff1589ab074e7615c47d00a11c941
unknown <nahidhasanbijoy26@gmail.com>
2025-05-15 01:09:58 +0600

    modified

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -5,30 +5,60 @@ import './App.css'
 
 
 
+
 function App() {
+  
+  const StudentInfo =[
+  {
+    name: 'rakib',
+    roll: 1250
+  },
+  {
+    name: 'rohim',
+    roll: 1260
+  },
+  {
+    name: 'korim',
+    roll: 1290
+  },
+  {
+    name: 'zabbar',
+    roll: 1300
+  }
+  ]
+
   return (
     <div>
     {/* <NavBar/>
-    <Banner/> */}
-    <Student name="Rohim" roll="1201"/>
+    <Banner/> 
+     <Student name="Rohim" roll="1201"/>
     <Student name="Korim" roll="1202"/>
-    <Student name="Josim" roll="1203"/>
+    <Student name="Josim" roll="1203"/> */}
+      {
+        StudentInfo.map((item)=>{
+          return <Student item={item}></Student>
+         
+        })
+      }
+      
+
     </div>
-  )
+  );
 }
 
-
-
-function Student (props) {
+ function Student ({item}) {
+  // const {name, roll} = item
   return (
-    <div>
+    <>
       <div className='info'>
-        <h2>Name: {props.name} </h2>
-        <h3>Roll: {props.roll} </h3>
+        <h2>Name: {item.name} </h2>
+        <h3>Roll: {item.roll} </h3>
       </div>
-    </div>
+    </>
   )
 }
+
+
 
 export default App
 
--- first-react/src/components/student.jsx
+++ first-react/src/components/student.jsx
@@ -1,15 +1,5 @@
-import React from 'react'
-
- function Student (props) {
-  return (
-    <div>
-      <div className='info'>
-        <h2>Name: {props.name} </h2>
-        <h3>Roll: {props.roll} </h3>
-      </div>
-    </div>
-  )
-}
+// import React from 'react'
 
 
-export default student
+
+// export default Student
